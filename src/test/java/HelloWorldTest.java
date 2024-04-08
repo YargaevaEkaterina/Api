22ce55fe86314515b2ccdae852231cfd54caf507
YargaevaEkaterina <e.yargaeva.kazanexpress@gmail.com>
2024-04-08 19:06:33 +0300

    hw test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld()
+    public void testHello()
     {
-        System.out.println("Hello World");
+        System.out.println("Hello from Ekaterina");
+    }
+
+    @Test
+    public void testGet()
+    {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
