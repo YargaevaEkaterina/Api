76e168c71dc42144563e0da8c09db6879273c69b
YargaevaEkaterina <e.yargaeva.kazanexpress@gmail.com>
2024-04-09 17:06:26 +0300

    add tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
     @Test
@@ -8,6 +12,83 @@ public class HelloWorldTest {
     {
         System.out.println("Hello from Ekaterina");
     }
+
+    @Test
+    public void testHelloApi()
+    {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "Ekaterina");
+
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer2 = response.get("answer2");
+        if (answer2 == null)
+        {
+            System.out.println("The key is absent");
+        }
+        else
+        {
+            System.out.println(answer2);
+        }
+    }
+
+    @Test
+    public void testGetRestAssured()
+    {
+        Response response = RestAssured
+                .given()
+                .queryParam("param1", "value1")
+                .queryParam("param2", "value2")
+                .get("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
+
+    @Test
+    public void testPostSimpleBody()
+    {
+        Response response = RestAssured
+                .given()
+                .body("param1=value1&param2=value2")
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
+
+    @Test
+    public void testPostJsonBody()
+    {
+        Response response = RestAssured
+                .given()
+                .body("{\"param1\":\"value1\",\"param2\":\"value2\"}")
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
+
+    @Test
+    public void testPostMapBody()
+    {
+        Map<String, String> body = new HashMap<>();
+        body.put("param1", "value1");
+        body.put("param2", "value2");
+
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+
+        response.print();
+    }
+
 
     @Test
     public void testGet()
